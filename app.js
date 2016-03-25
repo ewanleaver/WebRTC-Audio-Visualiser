e1f97ff3a0861f815ca6e17756bf286a11a3ec4d
Ewan Leaver <ewanleaver@gmail.com>
2016-03-25 13:25:42 +0900

    Implementation for audio calls on SkyWay (untested)

--- app.js
+++ app.js
@@ -4,6 +4,36 @@ $(document).ready(function() {
   navigator.getUserMedia = navigator.getUserMedia || navigator.mozGetUserMedia ||
                          navigator.webkitGetUserMedia || navigator.msGetUserMedia;
 
+  var peer = new Peer({
+    key: '8f4f7ea3-6341-483a-8ab1-f36865df0299',
+    debug: 3
+  });
+
+  peer.on('open', function(){
+    $('#my-id').text(peer.id);
+  });
+
+  // Receiving a call
+  peer.on('call', function(call) {
+    call.answer(window.localStream);
+    call.on('stream', function(stream){
+      $('#remoteAudio').prop('src', URL.createObjectURL(stream));
+    });
+    $('.call-ui').hide();
+  });
+
+  peer.on('error', function(err){
+    console.log(err.message);
+  });
+
+  // Click handlers setup
+  $(function(){
+    $('#make-call').click(function(){
+      // Initiate a call!
+      var call = peer.call($('#input-id').val(), window.localStream);
+    });
+  });
+
   var mediaConstraints = {
     video: false,
     audio: true
@@ -12,19 +42,22 @@ $(document).ready(function() {
   // Get audio stream
   navigator.getUserMedia(mediaConstraints, getUserMediaSuccess, getUserMediaError);
 
-  var audioContext;
-  var audioSource;
-  var analyser;
+  var localAudioContext;
+  var localAudioSource;
+  var localAnalyser;
 
   function getUserMediaSuccess(stream) {
-    audioContext = new (window.AudioContext || window.webkitAudioContext)();
-    audioSource = audioContext.createMediaStreamSource(stream);
-    // Used to retrieve frequency data
-    analyser = audioContext.createAnalyser();
+    window.localStream = stream;
+    $('#localAudio').prop('src', URL.createObjectURL(stream));
 
-    // Bind our analyser to the media element source
-    audioSource.connect(analyser);
-    audioSource.connect(audioContext.destination);
+    localAudioContext = new (window.AudioContext || window.webkitAudioContext)();
+    localAudioSource = localAudioContext.createMediaStreamSource(stream);
+    // Used to retrieve frequency data
+    localAnalyser = localAudioContext.createAnalyser();
+
+    // Bind our localAnalyser to the media element source
+    localAudioSource.connect(localAnalyser);
+    localAudioSource.connect(localAudioContext.destination);
 
     // Run the loop
     render();
@@ -35,43 +68,43 @@ $(document).ready(function() {
   }
 
   // Get frequency data and visualise it
-  var freqData = new Uint8Array(100);
-  var freqDataOne = new Uint8Array(1);
+  var localFreqData = new Uint8Array(100);
+  var localFreqDataOne = new Uint8Array(1);
 
-  var svgHeight = $(window).height();
-  var svgWidth = $(window).width();
+  var localSvgHeight = $(window).height();
+  var localSvgWidth = $(window).width();
   var barPadding = 1;
 
   function createSvg(parent, height, width) {
-    return d3.select(parent).append('svg').attr('height', height).attr('width', width);
+    return d3.select(parent).append('localSvg').attr('height', height).attr('width', width);
   }
 
-  var svg = createSvg('body', svgHeight, svgWidth);
+  var localSvg = createSvg('body', localSvgHeight, localSvgWidth);
 
   // Create our initial graph visualisation
   // We don't care about height and y attributes until music starts playing
-  svg.selectAll('rect')
-    .data(freqData)
+  localSvg.selectAll('rect')
+    .data(localFreqData)
     .enter()
     .append('rect')
     .attr('x', function(d, i) {
-      return i * (svgWidth / freqData.length);
+      return i * (localSvgWidth / localFreqData.length);
     })
-    .attr('width', svgWidth / freqData.length - barPadding);
+    .attr('width', localSvgWidth / localFreqData.length - barPadding);
 
   // Continuously loop and update with frequency data
   function render() {
     requestAnimationFrame(render);
 
-    // Copy frequency data to freqData array
-    analyser.getByteFrequencyData(freqData);
-    analyser.getByteFrequencyData(freqDataOne);
+    // Copy frequency data to localFreqData array
+    localAnalyser.getByteFrequencyData(localFreqData);
+    localAnalyser.getByteFrequencyData(localFreqDataOne);
 
-    // Update d3 graph with new data
-    svg.selectAll('rect')
-      .data(freqData)
+    // Update local d3 graph with new data
+    localSvg.selectAll('rect')
+      .data(localFreqData)
       .attr('y', function(d) {
-        return svgHeight/2-1.75*d;
+        return localSvgHeight/2-1.75*d;
       })
       .attr('height', function(d) {
         return 3.5*d;
@@ -81,12 +114,11 @@ $(document).ready(function() {
       });
 
     d3.select('body')
-      .data(freqDataOne)
+      .data(localFreqDataOne)
       .style('background-color', function(d) {
         var value = d*0.2;
         return 'rgb(' + value + ', ' + value + ', ' + d*0.5 + ')';
       });
-      //.style("background-color", "black"); 
   }
 
 });
